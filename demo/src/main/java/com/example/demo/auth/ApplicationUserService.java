7f170bfa9e3b8ce5b162478af93bcf7ef41a51ee
valberjr <3cifhzht@duck.com>
2021-12-14 09:47:14 -0300

    application user repository

--- demo/src/main/java/com/example/demo/auth/ApplicationUserService.java
+++ demo/src/main/java/com/example/demo/auth/ApplicationUserService.java
@@ -8,8 +8,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ApplicationUserService implements UserDetailsService {
 
+    private final ApplicationUserRepository applicationUserRepository;
+
+    public ApplicationUserService(ApplicationUserRepository applicationUserRepository) {
+        this.applicationUserRepository = applicationUserRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        return applicationUserRepository
+                .selectApplicationUserByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Username %s not found", username)));
     }
 }
